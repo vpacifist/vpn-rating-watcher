ff8d0a7ac39d7db89aac027982ed1d6e76a437c3
Vladimir Gusev <v@vovagusev.ru>
2026-04-07 18:43:15 +0200

    Move spline tests to dedicated file to avoid merge conflicts

--- tests/test_chart_aggregation.py
+++ tests/test_chart_aggregation.py
@@ -12,11 +12,9 @@ from sqlalchemy.orm import Session
 from vpn_rating_watcher.charts.service import (
     ChartRegenerationMetadata,
     DailyScoreRow,
-    _catmull_rom_segment,
     _compute_label_positions,
     _effective_chart_dates,
     _matrix_from_rows,
-    _smooth_curve_points,
     get_max_point_date,
     query_daily_aggregated_scores,
     regenerate_chart_to_temp_file,
@@ -612,39 +610,6 @@ def test_matrix_from_rows_keeps_missing_dates_as_gaps() -> None:
     assert matrix[vpn_a_idx, 0] == 30
     assert np.isnan(matrix[vpn_a_idx, 1])
     assert matrix[vpn_a_idx, 2] == 31
-
-
-def test_smooth_curve_points_returns_original_for_short_series() -> None:
-    x_values = np.array([0, 1], dtype=float)
-    y_values = np.array([21, 22], dtype=float)
-
-    smooth_x, smooth_y = _smooth_curve_points(x_values, y_values)
-
-    assert np.array_equal(smooth_x, x_values)
-    assert np.array_equal(smooth_y, y_values)
-
-
-def test_smooth_curve_points_keeps_endpoints_for_long_series() -> None:
-    x_values = np.array([0, 1, 2, 3], dtype=float)
-    y_values = np.array([22, 24, 23, 25], dtype=float)
-
-    smooth_x, smooth_y = _smooth_curve_points(x_values, y_values, steps_per_segment=5)
-
-    assert smooth_x[0] == x_values[0]
-    assert smooth_y[0] == y_values[0]
-    assert smooth_x[-1] == x_values[-1]
-    assert smooth_y[-1] == y_values[-1]
-    assert smooth_x.size > x_values.size
-    assert smooth_y.size > y_values.size
-    assert np.all(np.diff(smooth_x) > 0)
-
-
-def test_catmull_rom_segment_matches_segment_endpoints() -> None:
-    t_values = np.array([0.0, 1.0], dtype=float)
-    values = _catmull_rom_segment(10.0, 20.0, 30.0, 40.0, t_values)
-
-    assert values[0] == 20.0
-    assert values[1] == 30.0
 
 
 def test_compute_label_positions_preserves_order_and_spacing() -> None:
